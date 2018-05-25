c8eacdc3d0f5ae03b570c42ade664dd8dbcbefcb
denyle <dennyle@captivategrp.com>
2018-05-25 12:29:58 +0800

    inout

--- resources/views/inc/inout.blade.php
+++ resources/views/inc/inout.blade.php
@@ -1,55 +1,55 @@
-<div class="row">
-   <div class="col-md-9 offset-3">
+<div class="table100 ver1">
 <h2>Time Keeping Table</h2>
-<table>
-  <tr>
-    <th>DATE</th>
-    <th>TIME IN</th>
-    <th>TIME OUT</th>
-    <th>UNDERTIME</th>
-    <th>ADJUST</th>
+<table data-vertable="ver1">
+						<thead>
+							<tr class="row100 head">
+    <th class="column100 column2" data-column="column2"DATE</th>
+    <th class="column100 column3" data-column="column3"TIME IN</th>
+    <th class="column100 column4" data-column="column4"TIME OUT</th>
+    <th class="column100 column5" data-column="column5">UNDERTIME</th>
+    <th class="column100 column6" data-column="column6">ADJUST</th>
   </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-    <td>Germany</td>
-    <td>Germany</td>
+  <tr class="row100">
+    <td class="column100 column2" onclick="document.getElementById('link').click()" data-column="column2">Alfreds Futterkiste</td>
+    <td class="column100 column3" onclick="document.getElementById('link').click()" data-column="column3">Maria Anders</td>
+    <td class="column100 column4" onclick="document.getElementById('link').click()" data-column="column4">Germany</td>
+    <td class="column100 column5" onclick="document.getElementById('link').click()" data-column="column5">Germany</td>
+    <td class="column100 column6" onclick="document.getElementById('link').click()" data-column="column6">Germany</td>
   </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-    <td>Germany</td>
-    <td>Germany</td>
+  <tr class="row100">
+    <td class="column100 column2" onclick="document.getElementById('link').click()" data-column="column2">Centro comercial Moctezuma</td>
+    <td class="column100 column3" onclick="document.getElementById('link').click()" data-column="column3">Francisco Chang</td>
+    <td class="column100 column4" onclick="document.getElementById('link').click()" data-column="column4">Mexico</td>
+    <td class="column100 column5" onclick="document.getElementById('link').click()" data-column="column5">Germany</td>
+    <td class="column100 column6" onclick="document.getElementById('link').click()" data-column="column6">Germany</td>
   </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-    <td>Germany</td>
-    <td>Germany</td>
+  <tr class="row100">
+    <td class="column100 column2" onclick="document.getElementById('link').click()" data-column="column2">Ernst Handel</td>
+    <td class="column100 column3" onclick="document.getElementById('link').click()" data-column="column3">Roland Mendel</td>
+    <td class="column100 column4" onclick="document.getElementById('link').click()" data-column="column4">Austria</td>
+    <td class="column100 column5" onclick="document.getElementById('link').click()" data-column="column5">Germany</td>
+    <td class="column100 column6" onclick="document.getElementById('link').click()" data-column="column6">Germany</td>
   </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-    <td>Germany</td>
-    <td>Germany</td>
+  <tr class="row100">
+    <td class="column100 column2" onclick="document.getElementById('link').click()" data-column="column2">Island Trading</td>
+    <td class="column100 column3" onclick="document.getElementById('link').click()" data-column="column3">Helen Bennett</td>
+    <td class="column100 column4" onclick="document.getElementById('link').click()" data-column="column4">UK</td>
+    <td class="column100 column5" onclick="document.getElementById('link').click()" data-column="column5">Germany</td>
+    <td class="column100 column6" onclick="document.getElementById('link').click()" data-column="column6">Germany</td>
   </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-    <td>Germany</td>
-    <td>Germany</td>
+  <tr class="row100">
+    <td class="column100 column2" onclick="document.getElementById('link').click()" data-column="column2">Laughing Bacchus Winecellars</td>
+    <td class="column100 column3" onclick="document.getElementById('link').click()" data-column="column3">Yoshi Tannamuri</td>
+    <td class="column100 column4" onclick="document.getElementById('link').click()" data-column="column4">Canada</td>
+    <td class="column100 column5" onclick="document.getElementById('link').click()" data-column="column5">Germany</td>
+    <td class="column100 column6" onclick="document.getElementById('link').click()" data-column="column6">Germany</td>
   </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-    <td>Germany</td>
-    <td>Germany</td>
+  <tr class="row100">
+    <td class="column100 column2" onclick="document.getElementById('link').click()" data-column="column2">Magazzini Alimentari Riuniti</td>
+    <td class="column100 column3" onclick="document.getElementById('link').click()" data-column="column3">Giovanni Rovelli</td>
+    <td class="column100 column4" onclick="document.getElementById('link').click()" data-column="column4">Italy</td>
+    <td class="column100 column5" onclick="document.getElementById('link').click()" data-column="column5">Germany</td>
+    <td class="column100 column6" onclick="document.getElementById('link').click()" data-column="column6">Germany</td>
     
   </tr>
 </table>
